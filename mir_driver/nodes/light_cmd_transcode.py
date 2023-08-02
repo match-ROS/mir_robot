e17fd1cda2890a6688c79e1f8e5d0c33b4b53144
Hauke Heeren <heeren@match.uni-hannover.de>
2023-08-02 18:46:31 +0200

    first working version

--- mir_driver/nodes/light_cmd_transcode.py
+++ mir_driver/nodes/light_cmd_transcode.py
@@ -12,26 +12,14 @@ import mir_msgs.msg
 class LightCmdTranscode():
     def __init__(self):
         rospy.Subscriber('/new_light_cmd', mir_msgs.msg.LightCmd, self.callback)
-        self.light_cmd_srv = rospy.ServiceProxy('/light_cmd_srv', LightCommand)
+        self.light_cmd_srv = rospy.ServiceProxy('/light_srv', LightCommand)
         
         rospy.spin()
 
-
-
-
     def callback(self,data):
-        print(data)
-        light_cmd_req = LightCommand()
-        light_cmd_req.command = data.effect
-        light_cmd_req.color = data.color1
-        light_cmd_req.color2 = data.color2
-        light_cmd_req.leds = data.leds
-        light_cmd_req.token = data.token
-        light_cmd_req.timeout = data.timeout
-        light_cmd_req.priority = data.priority
 
-        print(light_cmd_req)
-        self.light_cmd_srv(data.effect, data.color1, data.color2, data.leds, data.token, data.timeout, data.priority)
+        result= self.light_cmd_srv(data.effect, data.color1, data.color2, data.leds, data.token, data.timeout, data.priority)
+        print(result)
 
 if __name__ == '__main__':
     try:
