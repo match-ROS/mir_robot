3c190a0f114a7ce49a2ce24374af153e4bb78c4c
Hauke Heeren <heeren@match.uni-hannover.de>
2023-08-03 18:48:56 +0200

    first working version

--- mir_driver/nodes/light_cmd_transcode.py
+++ mir_driver/nodes/light_cmd_transcode.py
@@ -5,25 +5,42 @@
 
 import rospy
 from mir_srvs.srv import LightCommand
-
-#from mir_msgs.msg import LightCommand
+import os
 import mir_msgs.msg
+from light_cmd_transcode_local import LightCmdTranscodeLocal
+import socket
+import json
 
 class LightCmdTranscode():
     def __init__(self):
-        rospy.Subscriber('/new_light_cmd', mir_msgs.msg.LightCmd, self.callback)
+        local_master_uri = "http://roscore:11311"
+        remote_master_uri = "http://192.168.12.20:11311"
+        self.sub_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sub_socket.connect(('localhost', 8000))
+        self.set_ros_master_uri(remote_master_uri)
+        rospy.init_node('light_cmd_transcode_remote')
+        rospy.wait_for_service('/light_srv')
+        rospy.loginfo("LightCmdTranscode: Service /light_srv is available")
         self.light_cmd_srv = rospy.ServiceProxy('/light_srv', LightCommand)
         
-        rospy.spin()
-
-    def callback(self,data):
-
-        result= self.light_cmd_srv(data.effect, data.color1, data.color2, data.leds, data.token, data.timeout, data.priority)
-        print(result)
+        self.main()
+        
+    def main(self):
+        rate = rospy.Rate(100) # 10hz
+        while not rospy.is_shutdown():
+            json_data = self.sub_socket.recv(1024).decode()
+            data_dict = json.loads(json_data)
+            data_list = [data_dict['effect'], data_dict['color1'], data_dict['color2'], data_dict['leds'],
+                     data_dict['token'], data_dict['timeout'], data_dict['priority']]
+            result = self.light_cmd_srv(*data_list)
+            rate.sleep()
+                    
+    def set_ros_master_uri(self,remote_uri):
+        os.environ["ROS_MASTER_URI"] = remote_uri
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('light_cmd_transcode', anonymous=True)
+        
         LightCmdTranscode()
     except rospy.ROSInterruptException:
         pass
