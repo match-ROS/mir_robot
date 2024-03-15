cc654665756b53cbca1ee3c902b30da744ab755b
Tobias Recker <Recker@match.uni-hannover.de>
2024-03-15 15:45:44 +0100

    enabled fake_joint_publisher

--- mir_driver/nodes/fake_mir_joint_publisher.py
+++ mir_driver/nodes/fake_mir_joint_publisher.py
@@ -35,9 +35,10 @@ from sensor_msgs.msg import JointState
 
 def fake_mir_joint_publisher():
     rospy.init_node('fake_mir_joint_publisher')
-    prefix = rospy.get_param('~prefix', '')
+    prefix = rospy.get_param('~tf_prefix', '')
+    prefix = ''
     pub = rospy.Publisher('joint_states', JointState, queue_size=10)
-    r = rospy.Rate(50.0)
+    r = rospy.Rate(5.0)
     while not rospy.is_shutdown():
         js = JointState()
         js.header.stamp = rospy.Time.now()
